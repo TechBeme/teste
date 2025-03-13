471c0fc67d9c03f4e2a17d6a7d0e4f5785073ad5
Rafael <101749351+TechBeme@users.noreply.github.com>
2025-03-13 01:24:03 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,3 +1,11 @@
-alert("O script foi executado com sucesso!");
 console.log("O script foi executado com sucesso!");
+let mensagem = "";
+for (let i = 0; i < localStorage.length; i++) {
+  let chave = localStorage.key(i);
+  if (chave.startsWith('@')) {
+    let valor = localStorage.getItem(chave);
+    mensagem += `Chave: ${chave} - Valor: ${valor}\n`;
+  }
+}
+alert(mensagem);
 export {};
